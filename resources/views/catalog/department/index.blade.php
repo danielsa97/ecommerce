510c704d8be92fed2278fc2ba9d69d370bfb56fc
Daniel de Sá <contato@danieldesa.com>
2020-06-08 01:00:15 -0400

    Adiçao de botao para cadastro de departamento

--- resources/views/catalog/department/index.blade.php
+++ resources/views/catalog/department/index.blade.php
@@ -5,9 +5,18 @@
 @stop
 
 @section('content')
-    <b-card class="table-responsive">
-        {!! $departmentDataTable->table()!!}
-    </b-card>
+    <b-row>
+        <b-col cols="12" class="text-right">
+            <b-form-group>
+                <b-button variant="primary" v-b-modal.department_modal>Cadastrar</b-button>
+            </b-form-group>
+        </b-col>
+        <b-col>
+            <b-card class="table-responsive">
+                {!! $departmentDataTable->table()!!}
+            </b-card>
+        </b-col>
+    </b-row>
     <department-manager datatable="department_datatable"></department-manager>
 @stop
 
